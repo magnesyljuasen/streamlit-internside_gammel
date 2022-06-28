e54b636daa7b2a517370928b7b23e8adf107ee1a
Magne Syljuåsen <66528035+magnesyljuasen@users.noreply.github.com>
2022-06-28 18:27:48 +0200

    Update geoteknikk.py

--- src/apps/geoteknikk.py
+++ src/apps/geoteknikk.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 
 def geoteknikk_app():
-    st.title("Geoteknikk")
+    st.title("Energibrønner og geoteknikk")
 
     st.header("Sjekkliste")
-    st.subheader("Grunnforhold")
+    st.subheader("Kartstudier")
     st.write("- *Dybde til fjell* | NADAG, GRANADA")
     st.write("- *Type løsmasser* | Løsmassedatabasen")
     st.write("- *Over/under marin grense* | Marin grense")
@@ -13,18 +13,22 @@ def geoteknikk_app():
     st.write("- *Terrengprofil* | Kartverket")
     st.write("- *Nærhet til hendelser vi vet om*")
     
+    st.subheader("f2")
+    
     st.subheader("Risiko")
     st.write("- Setningsskader eller skred?")
     st.write("- Type anlegg (åpent/lukket)")
-    st.write("- Størrelse på anlegg (antall brønner)")
-    st.write("- Løsmassemektighet") 
-    
-
-    
+    st.write("- Størrelse på anlegg (antall brønner) og temperaturnivåer")
+    st.write("- Løsmassemektighet og utstrekning") 
     
     st.subheader("Konsekvens")
-    
+    st.write("- Fundamentering bygg inkl. omkringliggende bygg (bunnledninger, gulv, pilarer, peler)") 
+    st.write("- Varierende dybder til fjell (skjevsetninger)")
+    st.write("- Store dybder til fjell (stort setningspotensiale)")
+
+
     st.subheader("Tiltak")
-    st.write("-  ")
+    st.write("- Utstøping")
+    st.write("- Poretrykksivå")
 
     
