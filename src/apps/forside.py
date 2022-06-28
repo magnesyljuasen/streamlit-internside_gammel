57c43efc9c711cbf0be01c388c40a4607bfdd18c
Magne Syljuåsen <66528035+magnesyljuasen@users.noreply.github.com>
2022-06-28 18:41:40 +0200

    Update forside.py

--- src/apps/forside.py
+++ src/apps/forside.py
@@ -53,7 +53,7 @@ def forside_app():
             return self.text
 
         def component(self):
-            return components.html(self.text, height=200)
+            return components.html(self.text, height=500)
 
 
     t = Tweet("https://twitter.com/sgehlin").component()
