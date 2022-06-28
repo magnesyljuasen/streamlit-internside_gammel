10efa24a01b0a0dadc87a2fb3b5e93ae77e8a403
Magne Syljuåsen <66528035+magnesyljuasen@users.noreply.github.com>
2022-06-28 18:15:04 +0200

    Update geoteknikk.py

--- src/apps/geoteknikk.py
+++ src/apps/geoteknikk.py
@@ -5,4 +5,20 @@ def geoteknikk_app():
     st.title("Geoteknikk")
 
     st.header("Sjekkliste")
-    st.write("1) Grunnforhold")
+    st.subheader("Grunnforhold")
+    st.write("- NADAG")
+    st.write("- GRANADA")
+    st.write("- Løsmassekart")
+    st.write("- Marin grense")
+    st.write("- Dypforvitring")
+    st.write("- Terrengprofil")
+    
+    st.subheader("Risiko")
+    
+    
+    st.subheader("Konsekvens")
+    
+    st.subheader("Tiltak")
+    st.write("-  ")
+
+    
