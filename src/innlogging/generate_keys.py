3b74f3d9d131058e7a7ea48d3b5a625c07b9ece7
magnesyljuasen <msylju@gmail.com>
2022-06-29 14:36:39 +0200

    d

--- src/innlogging/generate_keys.py
+++ src/innlogging/generate_keys.py
@@ -1,6 +1,6 @@
 import streamlit_authenticator as stauth
 
-hashed_passwords = stauth.Hasher(['asidjghiasdgjij', '456']).generate()
+hashed_passwords = stauth.Hasher(['GRUNNVARME123', 'GRUNNVARME123', 'GRUNNVARME123', 'GRUNNVARME123', 'GRUNNVARME123', 'GRUNNVARME123', 'GRUNNVARME123']).generate()
 print(hashed_passwords)
 
     
