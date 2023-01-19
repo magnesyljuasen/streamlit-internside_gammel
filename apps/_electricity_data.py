0e4ff885d1964bff95461cb30b850c4e0920d28a
magnesyljuasen <msylju@gmail.com>
2023-01-19 15:22:34 +0100

    elnett

--- apps/_electricity_data.py
+++ apps/_electricity_data.py
@@ -11,9 +11,10 @@ from shapely.geometry import Polygon
 @st.cache()
 def upload_data(number_of_points):
     df1 = pd.read_excel("src/data/input/el_nett_trondheim.xlsx", sheet_name=0)
+    df2 = pd.read_excel("src/data/input/el_nett_trondheim.xlsx", sheet_name=1)
     df1 = df1.head(number_of_points)
     trafo_list = df1["trafo"].unique()
-    return df1, trafo_list
+    return df1, df2, trafo_list
 
 @st.cache()
 def color_generator(trafo_list):
@@ -27,7 +28,7 @@ def power_grid():
     st.button("Refresh")
     st.title("Data fra Tensio")
     st.caption("Det er 115 118 adressepunkter i datasettet. Det tar lang tid å vise alle på kart (særlig for *adresser*)")
-    number_of_points = st.number_input("Antall punkter", value = 700, max_value=115118, step = 100)
+    number_of_points = st.number_input("Antall punkter", value = 100, max_value=115118, step = 100)
     #--
     st.markdown("*Velg type kartvisning*")
     c1, c2 = st.columns(2)
@@ -35,21 +36,32 @@ def power_grid():
         address_display = st.checkbox("Adresser", value=True)
     with c2:
         address_trafo_display = st.checkbox("Adresser tilknyttet samme trafo", value=True)
-    df, trafo_list = upload_data(number_of_points)
+    df1, df2, trafo_list = upload_data(number_of_points)
     color_list = color_generator(trafo_list)
     with st.expander("Data"):
-        st.write(df)
+        st.write(df1)
+        st.write(df2)
+    #--
     m = folium.Map(location=[63.446827, 10.421906], zoom_start=11)
     for i in range(0, len(trafo_list)):
-        df_plot = df.loc[df['trafo'] == trafo_list[i]]
+        df_plot = df1.loc[df1['trafo'] == trafo_list[i]]
         lat_point_list= df_plot["lat"]
         lng_point_list = df_plot["lng"]
         if len(lat_point_list) > 4:
+            trafo_name = df_plot["trafo"].iloc[0]
+            df2_row = df2.loc[df2["trafo"] == trafo_name]
             polygon_geom = Polygon(zip(lng_point_list, lat_point_list))
             polygon_geom2 = polygon_geom.convex_hull
             #--
+            text = f""" 
+            Trafo: <strong>{trafo_name}</strong></br> \
+            Nettstasjon: <strong>{df2_row["grid_station"].iloc[0]}</strong></br> \ 
+            Antall energimålere (AV | TRD): <strong>{len(lat_point_list)} | {df2_row["energimålere"].iloc[0]}</strong></br> \
+            Kapasitet (TRD): <strong>{round(df2_row["performance (kW)"].iloc[0] * 10/1000, 2)} GW</strong></br> \   
+            """
+            #--
             if address_trafo_display:
-                folium.GeoJson(polygon_geom2, style_function=lambda x: {'fillColor': 'orange'}, tooltip=df_plot["trafo"].iloc[0]).add_to(m)
+                folium.GeoJson(polygon_geom2, style_function=lambda x: {'fillColor': 'orange'}, tooltip=text).add_to(m)
             if address_display:
                 df_plot.apply(lambda row:folium.CircleMarker(location=[row["lat"], row["lng"]], radius=5, tooltip=row["trafo"], color=color_list[i]).add_to(m), axis=1)
     st_folium(m, width=725, returned_objects=[])
