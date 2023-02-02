9ee95bebbb180f568fb190a94e408000fa2a037b
magnesyljuasen <msylju@gmail.com>
2023-02-02 16:58:28 +0100

    ffffffffff

--- apps/_settings.py
+++ apps/_settings.py
@@ -5,7 +5,7 @@ import base64
 
 def settings():
     #st.set_page_config(page_title="AV Grunnvarme", page_icon=":bar_chart:", layout="centered")
-    st.set_page_config(page_title="AV Grunnvarme", layout="centered", page_icon="♨️")
+    st.set_page_config(page_title="AV Grunnvarme", layout="centered", page_icon="chart_with_upwards_trend")
 
     with open("src/styles/main.css") as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
