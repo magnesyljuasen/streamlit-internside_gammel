1d1e878fc7b2bd97bb7ee5939958c7814c3b1156
magnesyljuasen <msylju@gmail.com>
2023-02-02 20:10:19 +0100

    ffff

--- scripts/_ghetool.py
+++ scripts/_ghetool.py
@@ -87,10 +87,10 @@ class GheTool:
 #                st.metric("Aktive brønnmetere", value = f"{meters:,} m".replace(',', ' '))
 #                st.metric("ΔT", value = f"{delta_T:,} °C".replace(',', ' '))
 #            with c2:
-                st.metric("Energi per meter", value = f"{int(round(monthly_load_heating_sum/meters,0)):,} kWh/m".replace(',', ' '))
+            st.metric("Energi per meter", value = f"{int(round(monthly_load_heating_sum/meters,0)):,} kWh/m".replace(',', ' '))
 #                st.metric("Inn til varmepumpe", value = f"{round(min(borefield.results_peak_heating) + delta_T/2,1):,} °C".replace(',', ' '))
 #            with c3:
-                st.metric("Effekt per meter", value = f"{int((np.sum(self.peak_heating)*1000/meters,0)):,} W/m".replace(',', ' '))
+            st.metric("Effekt per meter", value = f"{int((np.sum(self.peak_heating)*1000/meters,0)):,} W/m".replace(',', ' '))
 #                st.metric("Ut fra varmepumpe", value = f"{round(min(borefield.results_peak_heating) - delta_T/2,1):,} °C".replace(',', ' '))
 
             
