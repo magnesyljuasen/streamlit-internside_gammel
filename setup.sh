11605968fa2286f7342dc33d3bedbf770c32c2d9
magnesyljuasen <msylju@gmail.com>
2023-02-03 09:49:28 +0100

    sdggds

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" >> ~/.streamlit/config.toml
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
